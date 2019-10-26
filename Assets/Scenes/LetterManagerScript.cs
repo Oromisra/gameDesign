16fc62c16290ad0368d1219000f39e550fed20ab
TownGame34 <56515400+TownGame34@users.noreply.github.com>
2019-10-26 13:00:58 +0200

    antoine changes
    Antoine changes

--- Assets/Scenes/LetterManagerScript.cs
+++ Assets/Scenes/LetterManagerScript.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class LetterManagerScript : MonoBehaviour
 {
-    public static char[] letterBase = { 'k', 'a', 'r', 'o', 's', 'h', 'i' };
+    public static char[] letterBase = { 'k', 'a', 'r', 'o' };
     public List<LetterDisplay> letterList;
     public LetterSpawner LetterSpawner;
     public int frequency = 100;
     private int time = 0;
-    public float stress = 0;
+    public int stress = 0;
     public StressBarScript stressBar;
-
+    public ScoreScript score;
     private void Start() {
     }
 
@@ -26,8 +26,10 @@ public class LetterManagerScript : MonoBehaviour
             if (this.letterList[0].getLetter() == letter) {
                 Destroy(this.letterList[0].gameObject);
                 this.letterList.RemoveAt(0);
+                score.scoreValue += 10;
             } else {
                 stressBar.addStress(0.02f);
+                score.scoreValue -= 2;
             }
         }
     }
@@ -38,6 +40,7 @@ public class LetterManagerScript : MonoBehaviour
                 Destroy(letter.gameObject);
                 this.letterList.Remove(letter);
                 stressBar.addStress(0.05f);
+                score.scoreValue -= 5;
                 return;
             }
         }
@@ -45,14 +48,12 @@ public class LetterManagerScript : MonoBehaviour
 
     private void Update() {
         if (!stressBar.isKaroshi()) {
-            stress = stressBar.getStress();
+
             checkForLetterLower();
             time++;
             if (time % frequency == 0) {
                 LetterDisplay newLetter = LetterSpawner.spawnLetter();
                 newLetter.setLetter(generateNextLetter());
-                newLetter.setSpeed(stress);
-                newLetter.setStressReference(stressBar);
                 letterList.Add(newLetter);
             }
         }
--- Assets/Scenes/LetterSpawner.cs
+++ Assets/Scenes/LetterSpawner.cs
@@ -9,6 +9,8 @@ public class LetterSpawner : MonoBehaviour
     public LetterDisplay spawnLetter() {
         Vector3 randomPosition = new Vector3(Random.Range(-50f, 50f), 35f);
 
+        Debug.Log(randomPosition);
+
         GameObject letterObj = Instantiate(letterPreFab, randomPosition, Quaternion.identity, letterCanvas);
         LetterDisplay letter = letterObj.GetComponent<LetterDisplay>();
         return letter;
