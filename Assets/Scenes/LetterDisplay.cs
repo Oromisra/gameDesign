6345c42bb9de880668a8586be205519f46300247
killian <killiangabriel2@gmail.com>
2019-10-26 13:18:39 +0200

    corrected scripts

--- Assets/Scenes/LetterDisplay.cs
+++ Assets/Scenes/LetterDisplay.cs
@@ -8,6 +8,9 @@ public class LetterDisplay : MonoBehaviour
     public Text text;
     private char letter;
     public float fallSpeed = 2;
+    public float slideDirection = 0;
+    public int time = 0;
+    public StressBarScript stressBar;
 
     public void setLetter(char letter) {
         this.letter = letter;
@@ -17,6 +20,15 @@ public class LetterDisplay : MonoBehaviour
     public char getLetter() {
         return letter;
     }
+
+    public void setStressReference(StressBarScript reference) {
+        stressBar = reference;
+    }
+
+    public void setSpeed(float speed) {
+        fallSpeed += speed;
+    }
+
 
     public bool needsToDestroy() {
         Vector3 p = transform.position;
@@ -29,6 +41,17 @@ public class LetterDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f, -fallSpeed, 0f);
+        time++;
+        float stress = stressBar.getStress();
+        if (stress >= 0.2f) {
+            if (time / 5 % 2 == 0) {
+                slideDirection = -2 - (stress * 2);
+            } else {
+                slideDirection = 2 + (stress * 2);
+            }
+        } else {
+            slideDirection = 0;
+        }
+        transform.Translate(slideDirection, -fallSpeed, 0f);
     }
 }
