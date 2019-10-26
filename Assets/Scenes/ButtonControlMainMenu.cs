a3f1f893962eba2999e85bb524ad0be5f373b076
TownGame34 <56515400+TownGame34@users.noreply.github.com>
2019-10-26 13:34:15 +0200

    Script Main Menu

---
+++ Assets/Scenes/ButtonControlMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonControlMainMenu : MonoBehaviour
+{
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("GameDayScene");
+    }
+
+    public void QuitBtn()
+    {
+        Application.Quit();
+    }
+}
